a2fb7bc4009c20b8510340905f7915b5af0a9f8e
Maxim <guromax11@gmail.com>
2024-11-11 17:52:57 +0300

    v2

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
 
+func main() {
+	bot, err := tgbotapi.NewBotAPI("7426266833:AAHHmqqOnr8026lgMe0grWHvf_yRS0PPY8A")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
